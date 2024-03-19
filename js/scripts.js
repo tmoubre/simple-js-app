bf10295375b30484832c53c7a93e8f0ab1f52330
tmoubre <160032580+tmoubre@users.noreply.github.com>
2024-03-19 08:00:09 -0500

    Created js / scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+alert('Hello World!')
+
+// let myName='Bob';
+// document.write(myName);
+
+// myName='John Doe';
+// document.write(myName);
+
+// let simpleAddition=2+2;
+// document.write(simpleAddition);
+
+// let size=100;
+// let doubleSize=size*2;
+// document.write(doubleSize);
+
+// let size =100;
+// let doubleSize=size*2;
+// let minSize=(doubleSize*2)-(size/2);
+// document.write(minSize);
+
+let favoriteFood='English Cuisine';
+document.write(favoriteFood);
