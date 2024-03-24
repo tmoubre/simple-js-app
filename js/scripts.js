25e8ae2ea86be362b4befac9dce7a3fa7d1ef2b1
tmoubre <160032580+tmoubre@users.noreply.github.com>
2024-03-24 12:34:49 -0500

    added notes /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList = [
+  //pokemon attributes
   {name: 'Charmander', type:'grass', height: 6},
   {Name: 'Venusar', type: ['grass', 'poison'], height: 2},
   {name: 'Ivysaur', type: ['grass','poison'],  height: 1}
