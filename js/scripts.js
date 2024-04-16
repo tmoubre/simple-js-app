e1bb1b58fa25a669fcdf8e434ebe4286d9eeb194
tmoubre <160032580+tmoubre@users.noreply.github.com>
2024-04-16 06:15:17 -0500

    Added loop for pokemon list / script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 let pokemonList = [
   //pokemon attributes
-  {name: 'Charmander', type:'grass', height: 6},
-  {Name: 'Venusar', type: ['grass', 'poison'], height: 2},
-  {name: 'Ivysaur', type: ['grass','poison'],  height: 1}
+  { name: 'Charmander', type: 'grass', height: 6 },
+  { name: 'Veusar',     type: ['grass', 'poison'], height: 2 },
+  { name: 'Ivysaur',    type: ['grass', 'poison'], height: 1 }
 ];
 
+for (let i = 0; i < pokemonList.length; i++) {
+
+  if (pokemonList[i].height == 6) {
+    //loop to determine the size and mae of the pokemon
+    document.write('<p>' + pokemonList[i].name + ' /'+' Height:' + pokemonList[i].height + ' - wow that is big! </p>')
+  } else {
+    document.write('<p>' + pokemonList[i].name + ' /' + ' Height:' + pokemonList[i].height + ' </p>')
+  }
+}
